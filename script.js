7953b7fb494bc15f1e20c567ca60e667beff2e23
brownie5253 <01connorbrowne@gmail.com>
2024-02-12 11:15:18 +1100

    Georgie changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+counter = 0
+
 function showMessage(response) {
   if (response === "No") {
     const noButton = document.getElementById("no-button");
@@ -8,20 +10,34 @@ function showMessage(response) {
     // Set button position to absolute
     noButton.style.position = "absolute";
 
+    if (counter < 5) {
+        counter += 1
+    }
+
     // Change image source to "gun.gif"
-    document.getElementsByClassName("image")[0].src = "images/gun.gif";
+    document.getElementsByClassName("image")[0].src = "images/" + "sad" + counter + ".jpeg";
 
     // Generate random coordinates within the visible container
     const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
     const randomY = Math.max(0, Math.floor(Math.random() * maxHeight));
 
+    if (counter == 5) {
+        noButton.style.display = 'none'
+    }
+
+
     // Apply new coordinates to the button
     noButton.style.left = randomX + "px";
     noButton.style.top = randomY + "px";
 
+    textMessage = "The no button is just for visuals";
+
+    if (counter == 5) {
+        textMessage = "I'm done askin...."
+    }
+
     // Update text content and hide name message
-    document.getElementById("question").textContent =
-      "The no button is just for visuals";
+    document.getElementById("question").textContent = textMessage;
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
@@ -34,10 +50,10 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.textContent = "LESGOOO see you on the 14th 😍😍😍";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "images/dance-dog.gif";
 
     // Remove yes button
     document.getElementById("yesButton").remove();
